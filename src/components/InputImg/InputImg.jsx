5266971cc7e4c966ae8dd7ce80df2870be591c7b
AndrMar1939 <advo737@gmail.com>
2023-03-13 19:46:42 +0200

    hw3

--- src/components/InputImg/InputImg.jsx
+++ src/components/InputImg/InputImg.jsx
@@ -3,20 +3,21 @@ import { AppContext } from '../../App';
 import './style.css';
 
 export default function InputImg () {
-    const {dispatch} = useContext(AppContext); 
-
+    const {state, dispatch} = useContext(AppContext); 
+    console.log(state)
     return (
         <div className='input-box'>
             <input 
                 className='input-img'
                 type="file"
-                accept="image/png, image/jpeg"                              
-                onChange={(e) =>  
-                            
+                accept="image/png, image/jpeg"                            
+                onChange={(e) =>  { 
+                    e.preventDefault()                           
                     dispatch({
                         type: 'DATA-INPUT',
                         payload: {imgSrc: URL.createObjectURL(e.target.files[0])}
                     })
+                }
                 }               
                 
             />            
